d9c23f6631f485795984b1010fc80efb19aa592d
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-03-07 12:43:16 +0700

    🧹 chore(server): init project

---
+++ src/main/java/com/uit/se109/Se109Application.java
@@ -0,0 +1,12 @@
+package com.uit.se109;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se109Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Se109Application.class, args);
+  }
+}
