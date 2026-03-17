73268fa7f750ecca23255b797344afd9d27c0f2c
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-03-17 15:23:35 +0700

    🚧 build(server): add spotless & sonarQube, testcontainer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
 	java
+    id("org.sonarqube") version "7.2.2.6593"
+    id("com.diffplug.spotless") version "8.2.1"
 	id("org.springframework.boot") version "4.0.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -20,8 +22,27 @@ configurations {
 	}
 }
 
+spotless {
+    java {
+        target("**/src/**/*.java")
+        googleJavaFormat()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
+
 repositories {
 	mavenCentral()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Phongdnh-uit_SE109")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "phongdnh")
+        property("sonar.coverage.jacoco.xmlReportPaths", "**/build/reports/jacoco/test/jacocoTestReport.xml")
+    }
 }
 
 dependencies {
@@ -31,6 +52,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.flywaydb:flyway-mysql")
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
@@ -41,6 +63,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers-mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
